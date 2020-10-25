cb84cfa8ddba23e5ce337f7f52a387bbe1d627f1
Damien008 <damien.lambert999@gmail.com>
2020-10-25 18:53:00 +0100

    premier commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new \Twig\Environment($loader, [
+       'cache' => false,
+   ]
+);
+
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+echo $twig->render('index.html.twig', array('products' => $products));
